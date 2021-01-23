852d50ab6fff3e0eba14397f46fa4f265e2c717c
Phil Bailey <philbailey@msn.com>
2021-01-22 19:43:46 -0500

    add Post to model and insert into seed.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 """Models for Blogly."""
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -11,7 +12,7 @@ def connect_db(app):
 
 
 class User(db.Model):
-    """User"""
+    """User Model"""
 
     __tablename__ = "users"
 
@@ -19,8 +20,28 @@ class User(db.Model):
     first_name = db.Column(db.String(20), nullable=False)
     last_name = db.Column(db.String(20), nullable=False)
     image_url = db.Column(db.String(150), nullable=False, default="/static/avatar-generic.png")
+    post_prox = db.relationship("Post")
+
+    def __repr__(self):
+        return f"id ={self.id} first_name={self.first_name} last_name={self.last_name} image_url={self.image_url}"
 
     @property
     def full_name(self):
         """ Return full name """
         return self.first_name + " " + self.last_name
+
+
+class Post(db.Model):
+    """Post Model"""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(20), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    user_prox = db.relationship("User")
+
+    def __repr__(self):
+        return f"id ={self.id} title={self.title} content={self.content} created_at={self.created_at} user_id={self.user_id}"
--- seed.py
+++ seed.py
@@ -1,6 +1,6 @@
 """Seed file to make sample data for db."""
 
-from models import db, User
+from models import db, User, Post
 from app import app
 
 # Create all tables
@@ -17,4 +17,18 @@ db.session.add(john)
 db.session.add(jane)
 db.session.add(jason)
 
+db.session.commit()
+
+post1 = Post(
+    title="Title of Post # 1",
+    content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec urna ac mi consequat tincidunt sed sed eros. Cras sit amet mi in enim dictum ultrices at sit amet nisi.",
+    user_id="1",
+)
+post2 = Post(
+    title="Title of Post # 2",
+    content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec urna ac mi consequat tincidunt sed sed eros. Cras sit amet mi in enim dictum ultrices at sit amet nisi.",
+    user_id="1",
+)
+db.session.add(post1)
+db.session.add(post2)
 db.session.commit()
