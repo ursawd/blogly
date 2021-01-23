1a409e6a781107577757b2508dbbbfb0d8e2e22c
Phil Bailey <philbailey@msn.com>
2021-01-23 15:25:53 -0500

    add routes and views for P1,P2,P3

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
 from flask import Flask, request, redirect, render_template
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///blogly"
@@ -70,6 +70,7 @@ def edit_form(user_id):
     """Edit user"""
     # retrieve user record matching passed in id
     user = User.query.get(user_id)
+
     # display user edit form
     return render_template("edit-user.html", user=user)
 
@@ -98,3 +99,33 @@ def delete(user_id):
     db.session.delete(user)  # uses record (not record id) to delete
     db.session.commit()
     return redirect("/users")
+
+
+# Route P1
+@app.route("/users/<int:user_id>/posts/new")
+def new_post_form(user_id):
+    """Display new post form"""
+    user = User.query.get(user_id)
+    return render_template("add-post.html", user=user)
+
+
+# Route P2
+@app.route("/users/<int:user_id>/posts/new", methods=["POST"])
+def new_post(user_id):
+    """Add new post"""
+    user = User.query.get(user_id)
+    title = request.form["title"]
+    content = request.form["content"]
+    fk = user_id
+    post = Post(title=title, content=content, user_id=fk)
+    db.session.add(post)
+    db.session.commit()
+    return render_template("detail-user.html", user=user)
+
+
+# Route P3
+@app.route("/posts/<int:post_id>")
+def show_post_form(post_id):
+    """Display post"""
+    post = Post.query.get(post_id)
+    return render_template("detail-post.html", post=post)
