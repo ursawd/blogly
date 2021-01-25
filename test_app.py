366add161f0decb8438c9ae181f952d1330b382e
Phil Bailey <philbailey@msn.com>
2021-01-24 20:35:54 -0500

    add tests for add user, add post, delete user and cascade delete post

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
 from app import app
 from unittest import TestCase
-from models import User
+from models import User, Post
 
 
 class UserTests(TestCase):
@@ -16,7 +16,7 @@ class UserTests(TestCase):
             resp = client.post(
                 "/users/new",
                 follow_redirects=True,
-                data={"fname": "AAA", "lname": "BBB", "imgurl": "/static/avatar-generic.png"},
+                data={"fname": "AAA", "lname": "BBB", "imgurl": "/static/imgs/avatar-generic.png"},
             )
             #
             html = resp.get_data(as_text=True)
@@ -32,7 +32,7 @@ class UserTests(TestCase):
             resp = client.post(
                 "/users/new",
                 follow_redirects=True,
-                data={"fname": "AAA", "lname": "BBB", "imgurl": "/static/avatar-generic.png"},
+                data={"fname": "AAA", "lname": "BBB", "imgurl": "/static/imgs/avatar-generic.png"},
             )
             # get test record to get record id
             user = User.query.filter_by(first_name="AAA").first()
@@ -52,12 +52,49 @@ class UserTests(TestCase):
             resp = client.post(
                 "/users/new",
                 follow_redirects=True,
-                data={"fname": "AAA", "lname": "BBB", "imgurl": "/static/avatar-generic.png"},
+                data={"fname": "AAA", "lname": "BBB", "imgurl": "/static/imgs/avatar-generic.png"},
             )
-            # get test reocrd to determine id
+            # get test record to determine id
             user = User.query.filter_by(first_name="AAA").first()
             # call delete route
             resp = client.get(f"/users/{user.id}/delete")
             html = resp.get_data(as_text=True)
             self.assertEqual(resp.status_code, 302)
             self.assertNotIn("AAA", html)
+
+    def test_add_post(self):
+        # adds test user to db, adds test post for that user,
+        # deletes users, cascade delete post for that user
+        # tests if neither user or post is in the db.
+        with app.test_client() as client:
+            # add new user to test against
+            resp = client.post(
+                "/users/new",
+                follow_redirects=True,
+                data={"fname": "AAA", "lname": "BBB", "imgurl": "/static/imgs/avatar-generic.png"},
+            )
+            # get test record to determine id
+            user = User.query.filter_by(first_name="AAA").first()
+
+            # add new post and test
+            resp = client.post(
+                f"/users/{user.id}/posts/new",
+                follow_redirects=True,
+                data={"title": "New Post", "content": "This is new post content", "user_id": user.id},
+            )
+
+            #
+            html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("New Post", html)
+            # break down: remove test record
+            user = User.query.filter_by(first_name="AAA").first()
+            # deleteing user should cascade delete posts
+            resp = client.get(f"/users/{user.id}/delete")
+            # operation should complete normally (302)
+            self.assertEqual(resp.status_code, 302)
+
+            user = User.query.filter_by(first_name="AAA").one_or_none()
+            self.assertIsNone(user)
+            post = Post.query.filter_by(title="New Post").one_or_none()
+            self.assertIsNone(post)
